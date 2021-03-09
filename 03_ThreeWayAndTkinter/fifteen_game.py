e2ad61e24026fcd94b859a57f606a0f7cc57e4ef
daria25D <dasha_domracheva@mail.ru>
2021-03-09 14:51:49 +0300

    Create initial layout of the game field
    Add buttons for numbers that should be arranged during the game.
    Position all buttons in the grid (positions are not random yet).

--- 03_ThreeWayAndTkinter/fifteen_game.py
+++ 03_ThreeWayAndTkinter/fifteen_game.py
@@ -8,10 +8,19 @@ class FifteenGame(tk.Frame):
 
 
     def createWidgets(self):
+        self.createGameField()
         self.startButton = tk.Button(self, text='New Game')
         self.quitButton  = tk.Button(self, text='Quit', command=self.quit)
-        self.startButton.grid()
-        self.quitButton.grid()
+        self.startButton.grid(row=4, column=0)
+        self.quitButton.grid(row=4, column=1)
+
+
+    def createGameField(self):
+        self.gameButtons = [tk.Button(self, text=str(i)) for i in range(1, 16)]
+        for i, button in enumerate(self.gameButtons):
+            row = i // 4
+            column = i % 4
+            button.grid(row=row, column=column, sticky=tk.W+tk.N+tk.S+tk.E)
 
 
 def main():
