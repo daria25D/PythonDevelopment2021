63f912e358eda247dd79acab05fcdfb52794fc8a
daria25D <dasha_domracheva@mail.ru>
2021-03-09 16:17:28 +0300

    Enable stretching of rows and columns

--- 03_ThreeWayAndTkinter/fifteen_game.py
+++ 03_ThreeWayAndTkinter/fifteen_game.py
@@ -3,16 +3,27 @@ import tkinter as tk
 class FifteenGame(tk.Frame):
     def __init__(self, master=None):
         tk.Frame.__init__(self, master)
-        self.grid()
+        self.grid(sticky='NSEW')
         self.createWidgets()
 
 
+    def enableStretching(self):
+        top = self.winfo_toplevel()
+        top.rowconfigure(0, weight=1)
+        top.columnconfigure(0, weight=1)
+        for i in range(4):
+            self.rowconfigure(i, weight=1, minsize=100, pad=2)
+            self.columnconfigure(i, weight=1, minsize=100, pad=2)
+        self.rowconfigure(4, weight=1, minsize=50)
+
+
     def createWidgets(self):
+        self.enableStretching()
         self.createGameField()
         self.startButton = tk.Button(self, text='New Game')
         self.quitButton  = tk.Button(self, text='Quit', command=self.quit)
-        self.startButton.grid(row=4, column=0)
-        self.quitButton.grid(row=4, column=1)
+        self.startButton.grid(row=4, column=0, columnspan=2, sticky='NSEW', padx=10, pady=10)
+        self.quitButton.grid(row=4, column=2, columnspan=2, sticky='NSEW', padx=10, pady=10)
 
 
     def createGameField(self):
@@ -20,7 +31,7 @@ class FifteenGame(tk.Frame):
         for i, button in enumerate(self.gameButtons):
             row = i // 4
             column = i % 4
-            button.grid(row=row, column=column, sticky=tk.W+tk.N+tk.S+tk.E)
+            button.grid(row=row, column=column, sticky='NSEW')
 
 
 def main():
