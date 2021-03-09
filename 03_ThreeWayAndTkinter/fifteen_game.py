da6c444e0f659069c3f255ffc2ac080308cf71ae
daria25D <dasha_domracheva@mail.ru>
2021-03-09 20:56:23 +0300

    Minor code reformatting
    Remove extra spacing.

--- 03_ThreeWayAndTkinter/fifteen_game.py
+++ 03_ThreeWayAndTkinter/fifteen_game.py
@@ -11,17 +11,15 @@ class FifteenGame(ttk.Frame):
         self.createWidgets()
         self.randomizeNewGame()
 
-
     def configureStyle(self):
         style = ttk.Style()
         style.configure('Game.TFrame', background='#e0e0f0')
         style.configure('Game.TButton', font=('Arial', 20), background='#ececf5', foreground='#121961', borderwidth='1')
         style.map('Game.TButton', foreground=[('active', '#3a71c9'), ('pressed', '#f0f4ff')],
-                                  background=[('active', '#f0f4ff'), ('pressed', '!focus', '#3a71c9')])
+                  background=[('active', '#f0f4ff'), ('pressed', '!focus', '#3a71c9')])
         style.configure('Control.TButton', font=('Helvetica', 18), background='#ececf5', foreground='#020951')
         style.map('Control.TButton', foreground=[('active', '#c97673'), ('pressed', '#a84d4a')],
-                                     background=[('active', '#fae9e8'), ('pressed', '!focus', '#fae9e8')])
-
+                  background=[('active', '#fae9e8'), ('pressed', '!focus', '#fae9e8')])
 
     def enableStretching(self):
         top = self.winfo_toplevel()
@@ -32,23 +30,20 @@ class FifteenGame(ttk.Frame):
             self.columnconfigure(i, weight=1, minsize=100, pad=2)
         self.rowconfigure(4, weight=1, minsize=60)
 
-
     def createWidgets(self):
         self.enableStretching()
         self.createGameFieldLayout()
         self.startButton = ttk.Button(self, text='NEW GAME', style='Control.TButton', command=self.randomizeNewGame)
-        self.quitButton  = ttk.Button(self, text='QUIT', style='Control.TButton', command=self.quit)
+        self.quitButton = ttk.Button(self, text='QUIT', style='Control.TButton', command=self.quit)
         self.startButton.grid(row=4, column=0, columnspan=2, sticky='NSEW', padx=10, pady=10, ipady=5)
         self.quitButton.grid(row=4, column=2, columnspan=2, sticky='NSEW', padx=10, pady=10, ipady=5)
-
 
     def createGameFieldLayout(self):
         self.currentPositions = [i for i in range(15)]  # currentPositions[buttonIdx] = coord
         self.blankPosition = 15
         self.gameButtons = [ttk.Button(self, text=str(i), style='Game.TButton') for i in range(1, 16)]
         for i, button in enumerate(self.gameButtons):
-            button.configure(command= lambda idx=i: self.moveButton(idx))
-
+            button.configure(command=lambda idx=i: self.moveButton(idx))
 
     def moveButton(self, idx):
         pos = self.currentPositions[idx]
@@ -77,7 +72,6 @@ class FifteenGame(ttk.Frame):
 
         self.checkWinning()
 
-
     def checkWinning(self):
         isWin = True
         for i in range(15):
@@ -86,7 +80,6 @@ class FifteenGame(ttk.Frame):
                 break
         if isWin:
             tk.messagebox.showinfo(message='You won!')
-
 
     def randomizeNewGame(self):
         numSteps = random.randint(10, 80)
@@ -108,16 +101,17 @@ class FifteenGame(ttk.Frame):
             elif blank_row == 3:
                 newPossibleBlankPositions.append(self.blankPosition - 4)
 
-            newBlankPosition = random.choice(newPossibleBlankPositions) #coord of button
+            newBlankPosition = random.choice(newPossibleBlankPositions)  # coord of button
             idx = self.currentPositions.index(newBlankPosition)
             self.currentPositions[idx] = self.blankPosition
             self.blankPosition = newBlankPosition
 
-        #reposition buttons
+        # reposition buttons
         for i, button in enumerate(self.gameButtons):
             row = self.currentPositions[i] // 4
             column = self.currentPositions[i] % 4
             button.grid(row=row, column=column, sticky='NSEW')
+
 
 def main():
     game = FifteenGame()
