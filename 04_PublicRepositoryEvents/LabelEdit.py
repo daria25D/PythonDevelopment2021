8a20253a29dfac9259799651de929ee05e73e08f
daria25D <dasha_domracheva@mail.ru>
2021-03-20 12:31:30 +0300

    Make Cursor substitute movable
    Move cursor if something has been entered into InoutLabel or it has been moved by Left/Right/Home/End keys. New position is calculated considering font.

--- 04_PublicRepositoryEvents/LabelEdit.py
+++ 04_PublicRepositoryEvents/LabelEdit.py
@@ -35,6 +35,11 @@ class InputLabel(tk.Label):
     def deactivate_cursor(self, event):
         self.Frame.configure(bg='#333333')
 
+    def move_cursor(self):
+        s = self.stringVar.get()[:self.cursorPosition]
+        textLengthInPixels = self.labelFont.measure(s)
+        self.Frame.place(x=textLengthInPixels)
+
     def on_key_pressed(self, event):
         if self.focus_get() is self:
             if event.char.isprintable() and len(event.char) > 0:
@@ -42,10 +47,13 @@ class InputLabel(tk.Label):
                 self.stringVar.set(s[:self.cursorPosition] + event.char + s[self.cursorPosition:])
                 self.cursorPosition += 1
                 self.configure(width=max(self.initialWidth, len(s) + 1))
+                self.move_cursor()
             if event.keysym == 'Left' or event.keysym == 'Home':
                 self.cursorPosition = max(self.cursorPosition - 1, 0)
+                self.move_cursor()
             if event.keysym == 'Right' or event.keysym == 'End':
                 self.cursorPosition = min(self.cursorPosition + 1, len(self.stringVar.get()))
+                self.move_cursor()
 
 
 class Application(tk.Frame):
