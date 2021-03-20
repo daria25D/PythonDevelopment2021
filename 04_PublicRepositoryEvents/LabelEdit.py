eea2a39a5783fbdeb9821516db909c369281f2f8
daria25D <dasha_domracheva@mail.ru>
2021-03-20 11:39:14 +0300

    Add event on keyboard keys pressed, change cursor position on keys pressed
    If InputLabel is in focus, the text variable assisiated with it can be changed by pressing keys (only printable keys affect it). Cursor position can be controlled by left and right arrows, as well as Home and End keys.

--- 04_PublicRepositoryEvents/LabelEdit.py
+++ 04_PublicRepositoryEvents/LabelEdit.py
@@ -2,14 +2,29 @@ import tkinter as tk
 
 class InputLabel(tk.Label):
     def __init__(self, master):
+        self.stringVar = tk.StringVar()
+        self.cursorPosition = 0
         tk.Label.__init__(self, master,
                           cursor='xterm',
                           highlightthickness=1,
                           padx=2,
                           pady=2,
                           takefocus=1,
+                          textvariable=self.stringVar,
                           width=20)
         self.master = master
+        self.bind('<Key>', self.on_key_pressed)
+
+    def on_key_pressed(self, event):
+        if self.focus_get() is self:
+            if event.char.isprintable() and len(event.char) > 0:
+                s = self.stringVar.get()
+                self.stringVar.set(s[:self.cursorPosition] + event.char + s[self.cursorPosition:])
+                self.cursorPosition += 1
+            if event.keysym == 'Left' or event.keysym == 'Home':
+                self.cursorPosition = max(self.cursorPosition - 1, 0)
+            if event.keysym == 'Right' or event.keysym == 'End':
+                self.cursorPosition = min(self.cursorPosition + 1, len(self.stringVar.get()))
 
 
 class Application(tk.Frame):
