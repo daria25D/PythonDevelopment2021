a883c611c4c8a6edc30f05b9fe0b12b3a1114db0
daria25D <dasha_domracheva@mail.ru>
2021-04-03 10:57:55 +0300

    Make application widgets stretchable

--- 05_SshAndSmartWidgents/TkGraphEdit.py
+++ 05_SshAndSmartWidgents/TkGraphEdit.py
@@ -5,17 +5,28 @@ class GraphEdit(tk.Frame):
     def __init__(self, master=None):
         tk.Frame.__init__(self, master)
         self.createWidgets()
-        self.grid()
+        self.enableStretching()
+        self.grid(sticky='NSEW')
 
     def createWidgets(self):
         self.textEditor = tk.Text(self, undo=True, wrap=tk.WORD)
         self.colorPicker = tk.Button(self, text='Color')
         self.quitButton = tk.Button(self, text='Quit', command=self.quit)
         self.graphEditor = tk.Canvas(self)
-        self.textEditor.grid(row=0, column=0, rowspan=2)
-        self.colorPicker.grid(row=0, column=1)
-        self.quitButton.grid(row=0, column=2)
-        self.graphEditor.grid(row=1, column=1, columnspan=2)
+        self.textEditor.grid(row=0, column=0, rowspan=2, sticky='NSEW')
+        self.colorPicker.grid(row=0, column=1, sticky='NSEW')
+        self.quitButton.grid(row=0, column=2, sticky='NSEW')
+        self.graphEditor.grid(row=1, column=1, columnspan=2, sticky='NSEW')
+
+    def enableStretching(self):
+        top = self.winfo_toplevel()
+        top.rowconfigure(0, weight=1)
+        top.columnconfigure(0, weight=1)
+        self.rowconfigure(0, weight=1, minsize=60, pad=2)
+        self.columnconfigure(0, weight=1, minsize=100, pad=2)
+        self.rowconfigure(1, weight=1, minsize=100)
+        self.columnconfigure(1, weight=1, minsize=50, pad=2)
+        self.columnconfigure(2, weight=1, minsize=50, pad=2)
 
 
 def main():
